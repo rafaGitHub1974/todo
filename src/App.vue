b78a8026d5b81096b775a45763a3f0694f3bd8f1
rafaGitHub1974 <esrafaelaliende@gmail.com>
2021-10-13 07:32:30 +0200

    fix: redirect to github repository

--- src/App.vue
+++ src/App.vue
@@ -5,34 +5,13 @@
       color="primary"
       dark
     >
-      <div class="d-flex align-center">
-        <!-- <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        /> -->
-
-        <!-- <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        /> -->
-      </div>
-
-      <v-spacer></v-spacer>
 
       <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        href="https://github.com/rafaGitHub1974/todo/"
         target="_blank"
         text
       >
-        <span class="mr-2">TODO LIST</span>
+        <span class="mr-2">TODO LIST (repository)</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
     </v-app-bar>
